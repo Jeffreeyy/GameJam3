4f3ea7046d0af10f6ea5e3923bf5e88972b29ae1
DasArmin <armin.hunter@hotmail.com>
2016-04-11 12:08:59 +0200

    Timing and ground check

---
+++ GameJam/Assets/Scripts/Timing.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timing : MonoBehaviour
+{
+    [SerializeField] private bool canPress;
+    private float time = 1;
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll != null)
+        {
+            print("found floor!");
+            StartCoroutine(PressingTime());
+        }
+    }
+
+    IEnumerator PressingTime()
+    {
+        print("press now");
+        canPress = true;
+
+        yield return new WaitForSeconds(time);
+        print("cant press");
+        canPress = false;
+    }
+}
