c34fd219cffbf9b33a7f8b071b86daae5d754138
DasArmin <armin.hunter@hotmail.com>
2016-04-11 13:51:13 +0200

    Player 1 & 2 can run a function when they collide with a floor

--- GameJam/Assets/Scripts/Input/Player1Input.cs
+++ GameJam/Assets/Scripts/Input/Player1Input.cs
@@ -1,9 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Player1Input : MonoBehaviour 
+public class Player1Input : MonoBehaviour
 {
     private PlayerMovement _playerMovement;
+    private Timing timing;
+
+    void Awake()
+    {
+        timing = GetComponent<Timing>();
+    }
 
 	// Use this for initialization
 	void Start () 
@@ -17,19 +23,38 @@ public class Player1Input : MonoBehaviour
         //Player 1
         if (Input.GetKeyDown(KeyCode.W))
         {
-            _playerMovement.Jump();
+            if (timing._isRed)
+                print("P1 got red!");
+
+            else if (!timing._isRed)
+                print("P1 Failed");
         }
+
         if (Input.GetKeyDown(KeyCode.A))
         {
-            //
+            if (timing._isGreen)
+                print("P1 got green!");
+
+            else if (!timing._isGreen)
+                print("P1 Failed");
         }
+
         if (Input.GetKeyDown(KeyCode.S))
         {
-            //
+            if (timing._isBlue)
+                print("P1 got blue!");
+
+            else if (!timing._isBlue)
+                print("P1 Failed");
         }
+
         if (Input.GetKeyDown(KeyCode.D))
         {
-            //
+            if (timing._isYellow)
+                print("P1 got yellow!");
+
+            else if (!timing._isYellow)
+                print("P1 Failed");
         }
 	}
 }
--- GameJam/Assets/Scripts/Input/Player2Input.cs
+++ GameJam/Assets/Scripts/Input/Player2Input.cs
@@ -1,35 +1,60 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Player2Input : MonoBehaviour 
+public class Player2Input : MonoBehaviour
 {
     private PlayerMovement _playerMovement;
+    private Timing timing;
+
+    void Awake()
+    {
+        timing = GetComponent<Timing>();
+    }
 
     // Use this for initialization
     void Start()
     {
         _playerMovement = GetComponent<PlayerMovement>();
-	}
-	
-	// Update is called once per frame
-	void Update () 
+    }
+
+    // Update is called once per frame
+    void Update()
     {
         //Player 2
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            _playerMovement.Jump();
+            if (timing._isRed)
+                print("P2 got red!");
+
+            else if (!timing._isRed)
+                print("P2 Failed");
         }
+
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            //
+            if (timing._isGreen)
+                print("P2 got green!");
+
+            else if (!timing._isGreen)
+                print("P2 Failed");
         }
+
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            //
+            if (timing._isBlue)
+                print("P2 got blue!");
+
+            else if (!timing._isBlue)
+                print("P2 Failed");
         }
+
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            //
+            if (timing._isYellow)
+                print("P2 got yellow!");
+
+            else if (!timing._isYellow)
+                print("P2 Failed");
         }
-	}
+    }
 }
