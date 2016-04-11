006af7e0afa165d668adc02f1ce4df733e98891e
DasArmin <armin.hunter@hotmail.com>
2016-04-11 14:58:34 +0200

    button Art

--- GameJam/Assets/Scripts/Timing.cs
+++ GameJam/Assets/Scripts/Timing.cs
@@ -8,6 +8,10 @@ public class Timing : MonoBehaviour
     public bool _isBlue;
     public bool _isYellow;
     private float time = 1;
+    public GameObject buttonRed;
+    public GameObject buttonGreen;
+    public GameObject buttonBlue;
+    public GameObject buttonYellow;
 
     void OnCollisionEnter2D(Collision2D coll)
     {
@@ -27,33 +31,41 @@ public class Timing : MonoBehaviour
 
     IEnumerator PressRed()
     {
+        buttonRed.SetActive(true);
         _isRed = true;
 
         yield return new WaitForSeconds(time);
         _isRed = false;
+        buttonRed.SetActive(false);
     }
 
     IEnumerator PressGreen()
     {
+        buttonGreen.SetActive(true);
         _isGreen = true;
 
         yield return new WaitForSeconds(time);
         _isGreen = false;
+        buttonGreen.SetActive(false);
     }
 
     IEnumerator PressBlue()
     {
+        buttonBlue.SetActive(true);
         _isBlue = true;
 
         yield return new WaitForSeconds(time);
         _isBlue = false;
+        buttonBlue.SetActive(false);
     }
 
     IEnumerator PressYellow()
     {
+        buttonYellow.SetActive(true);
         _isYellow = true;
 
         yield return new WaitForSeconds(time);
         _isYellow = false;
+        buttonYellow.SetActive(false);
     }
 }
