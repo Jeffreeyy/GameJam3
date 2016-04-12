ab290e47c9bfe6e44f63dae0c30024f731463143
samjerry <jerry@Cant-Touch-This-Macbook.local>
2016-04-12 11:59:39 +0200

    menu

---
+++ GameJam/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	public string startLevel;
+	public string Instructions;
+
+	public GameObject InstructionsScreen;
+	public GameObject MainMenuScreen;
+
+	public void startGame()
+	{
+		Application.LoadLevel (startLevel);
+	}
+
+	public void instructionsGame()
+	{
+		InstructionsScreen.SetActive (true);
+		MainMenuScreen.SetActive (false);
+	}
+
+	public void quitGame()
+	{
+		Debug.Log ("Game Exited");
+		Application.Quit ();
+
+	}
+
+	public void ReturnGame()
+	{
+		InstructionsScreen.SetActive (false);
+		MainMenuScreen.SetActive (true);
+	}
+}
---
+++ GameJam/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInput : MonoBehaviour 
+{
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+}
